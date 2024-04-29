99e687be5d9296834f2f91b50c6656ee3d651e1a
ShreetikaShrestha <shreetika592gmail.com>
2024-04-29 07:57:46 +0545

    1

---
+++ Week1/PrePostfix.java
@@ -0,0 +1,16 @@
+package Task.Week1;
+
+public class PrePostfix {
+    public static void main(String[] args) {
+        
+       // prefix operator
+       int prefixValue=10;
+       System.out.println("prefix value update the value in this line"+ ++prefixValue);
+       System.out.println("it remain same in next line "+ prefixValue);
+       //postfix operator
+       int postFixValue=20;
+       System.out.println("postfix value updates the value in next linme"+ postFixValue);
+       System.out.println("the value change in this line "+ postFixValue);
+    }
+    
+}
