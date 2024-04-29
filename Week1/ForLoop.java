d60378d96e4705294d61d90a3b328cf0b92280a1
ShreetikaShrestha <shreetika592gmail.com>
2024-04-29 08:00:59 +0545

    Week 3

---
+++ Week1/ForLoop.java
@@ -0,0 +1,10 @@
+package Task.Week1;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int index =0 ; index< 4 ; index ++){
+            System.out.println("looping "+index);
+        }    
+    }
+    
+}
